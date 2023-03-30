42d815955bb4086b10284d44dc2de909ab7a5bf1
Jakob Ruckel <jakob@ruckel.de>
2023-03-30 19:04:54 +0200

    Set image to anilist cover image

--- anipresence.py
+++ anipresence.py
@@ -4,24 +4,35 @@ import time
 from pypresence import Presence
 import os
 import re
+import requests
+import json
 
 
 class AniCliRPC:
     anime = None
     mpv_re = re.compile(
-        r".*mpv.*--force-media-title=(.*)\(.*episodes.*episode-([^-]+).*"
+        r".*mpv.*--force-media-title=(?P<title>.*) \((?P<epcount>[0-9]+) "
+        r"episodes.*episode-(?P<ep>[^-]+).*"
     )
     mpv_re_alt = re.compile(
-        r".*mpv.*--force-media-title=(.*)-episode-([^-]+).*"
+        r".*mpv.*--force-media-title=(?P<title>.*)-episode-(?P<ep>[^-]+).*"
     )
+    CACHE_PATH = os.path.expanduser("~/.cache/anipresence/cover.json")
+    cache = None
 
     def get_anime(self):
         ps = os.popen("ps aux").read()
         for line in ps.splitlines():
-            if m := self.mpv_re.match(line):
-                return m.groups(), False
-            if m := self.mpv_re_alt.match(line):
-                return m.groups(), True
+            if m := self.mpv_re.fullmatch(line):
+                return (
+                    (m.group("title"), m.group("ep"), m.group("epcount")),
+                    False,
+                )
+            if m := self.mpv_re_alt.fullmatch(line):
+                return (
+                    (m.group("title"), m.group("ep"), None),
+                    False,
+                )
         return None, None
 
     def update(self):
@@ -35,12 +46,12 @@ class AniCliRPC:
 
         self.anime = anime_new
 
-        title, ep = self.anime
+        title, ep, epcount = self.anime
 
         if hyphenated:
             title = " ".join([word.capitalize() for word in title.split("-")])
 
-        print(f"Watching {title} episode {ep}")
+        print(f"Watching {title} episode {ep}, epcount {epcount}")
 
         client_id = "908703808966766602"
 
@@ -51,7 +62,7 @@ class AniCliRPC:
         RPC.update(
             details=f"{title}",
             state=f"Episode {ep}",
-            large_image="watching",
+            large_image=self.try_get_cover_image_url(title, epcount),
             start=int(time.time()),
         )
         print("Updated RPC")
@@ -59,6 +70,7 @@ class AniCliRPC:
         return True
 
     def try_update(self):
+        self.update()
         try:
             print("Updating")
             return self.update()
@@ -69,6 +81,101 @@ class AniCliRPC:
     def loop(self):
         while self.try_update():
             time.sleep(20)
+
+    def try_get_cover_image_url(
+        self, title, epcount, fallback="watching"
+    ) -> str:
+        try:
+            return self.get_cover_image_url(title, epcount, fallback)
+        except Exception as e:
+            print(e)
+            return fallback
+
+    def get_cover_image_url(self, title, epcount, fallback="watching") -> str:
+        key = f"{title} -- {epcount}"
+        if not self.cache:
+            cachedir = os.path.dirname(self.CACHE_PATH)
+            if not os.path.exists(cachedir):
+                os.mkdir(cachedir)
+            if os.path.exists(self.CACHE_PATH):
+                with open(self.CACHE_PATH, "r") as f:
+                    self.cache = json.loads(f.read())
+            else:
+                with open(self.CACHE_PATH, "w+") as f:
+                    self.cache = {}
+                    f.write(json.dumps(self.cache))
+
+        if key not in self.cache.keys():
+            self.cache[key] = self._get_cover_image_url(
+                title, epcount, fallback
+            )
+            with open(self.CACHE_PATH, "w+") as f:
+                f.write(json.dumps(self.cache))
+
+        return self.cache[key]
+
+    def _get_cover_image_url(self, title, epcount, fallback="watching") -> str:
+        if epcount is not None:
+            epcount = int(epcount)
+
+        query = """
+            query($title: String) {
+              Page(page: 1, perPage: 10) {
+                media(search: $title, type: ANIME) {
+                  title {
+                    romaji
+                    english
+                  }
+                  episodes
+                  coverImage {
+                    medium
+                  }
+                }
+              }
+            }
+        """
+        variables = {"title": title}
+        url = "https://graphql.anilist.co"
+
+        print("Requesting", variables)
+        resp = requests.post(
+            url, json={"query": query, "variables": variables}
+        )
+        print(resp.text)
+        if not resp.status_code == 200:
+            return fallback
+
+        animes = list(resp.json()["data"]["Page"]["media"])
+        anime = None
+
+        if len(animes) > 1:
+            filtered_a = list(
+                filter(
+                    lambda a: a["title"]["romaji"].lower() == title.lower()
+                    and a["episodes"] == epcount,
+                    animes,
+                )
+            )
+            filtered_b = list(
+                filter(
+                    lambda a: a["title"]["romaji"].lower() == title.lower(),
+                    animes,
+                )
+            )
+            filtered_c = list(
+                filter(lambda a: a["episodes"] == epcount, animes)
+            )
+
+            for li in [filtered_a, filtered_b, filtered_c]:
+                if len(li) == 1:
+                    anime = li[0]
+                    break
+        elif len(animes) == 1:
+            anime = animes[0]
+
+        if anime is not None:
+            return anime["coverImage"]["medium"]
+        return fallback
 
 
 def main():
