954a4278a7c386053848cbf80db9c58e5a908924
jreeee <jeremias.miachel.kilian@uni-weimar.de>
2024-04-22 00:58:42 +0200

    ani-hist now works even if there is no histfile present

--- ani-hist.py
+++ ani-hist.py
@@ -40,18 +40,25 @@ def get_al_list(username, medium="ANIME", status="CURRENT"):
         url = 'https://graphql.anilist.co'
 
         response = requests.post(url, json={'query': query, 'variables': variables})
-        
+
         if not response.status_code == 200:
            print(str(response.status_code) + " could not retrieve data\n" + response.text)
            return
         return json.loads(response.text)["data"]["MediaListCollection"]["lists"][0]["entries"]
 
-def get_ac_list(hist_path):
+def get_ac_list(hist_path, hist_file):
     tmp_res = []
-    if not os.path.isfile(hist_path):
+    if not os.path.isfile(hist_path + hist_file):
         print("no histfile found, path is " + hist_path)
-        return
-    with open(hist_path) as hsf:
+        res = input("would you like to create a file at that location? [y/N]")
+        if res == "N" or res == "n" or res == "":
+            sys.exit(1)
+        if not os.path.exists(hist_path):
+            os.makedirs(hist_path)
+        with open(hist_path + hist_file, 'w') as nf:
+            nf.write("")
+        return tmp_res
+    with open(hist_path + hist_file) as hsf:
         for line in hsf:
             # make it presentable
             tmp = line.split("\t")
@@ -88,23 +95,24 @@ def update_list(ale, hl):
 
 def main():
     # hardcoded for now, can also be found with grep ^histfile $(which ani-cli)
-    ac_hist = os.path.expanduser("~/.local/state/ani-cli/ani-hsts")
-    ac_hist_old = os.path.expanduser("~/.local/state/ani-cli/ani-hsts.bak")
+    ac_hist_path = os.path.expanduser("~/.local/state/ani-cli/")
+    ac_hist_file ="ani-hsts"
+    ac_hist_old = "ani-hsts.bak"
     # specify your username via arg1 (./ani-hist.py testuser)
     user = sys.argv[1]
     al_entries = get_al_list(user)
-    ac_entries = get_ac_list(ac_hist)
+    ac_entries = get_ac_list(ac_hist_path, ac_hist_file)
     hash_list = get_hash(al_entries, ac_entries)
 
     new_entries = update_list(al_entries, hash_list)
 
     # append titles to backup file
-    with open(ac_hist_old, "a") as bak:
+    with open(ac_hist_path + ac_hist_old, "a") as bak:
         for i in ac_entries:
             bak.write(str(i[0]) + "\t" + str(i[1]) + "\t" + str(i[2]))
 
     # overwrite hist
-    with open(ac_hist, "w") as newhs:
+    with open(ac_hist_path + ac_hist_file, "w") as newhs:
         for i in new_entries:
             newhs.write(i)
 
